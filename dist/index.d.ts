7559e2c04bbabe099f2d248bcf600836f83e7285
kushhingol <kushhingol>
2021-06-15 16:00:57 +0530

    made more efficient

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const getIterableArray: (anyArray: Array<any>) => Array<any>;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @desc: Function is defined to yield data lazily (Generator function)
  * @param anyArray : Array<any>
  */
-function* processArrayList(anyArray: Array<any>) {
+function* processArrayList(anyArray) {
     const arrayLength = getArrayLength(anyArray);
     if(arrayLength) {
         let i =0;
@@ -19,12 +19,12 @@ function* processArrayList(anyArray: Array<any>) {
  * @param anyArray : Array<any>
  * @returns Number
  */
-const getArrayLength = (anyArray: Array<any>) : Number => {
+const getArrayLength = (anyArray) => {
     return anyArray.length;
 }
 
 
-export const getIterableArray = (anyArray: Array<any>) : Array<any> => {
+const getIterableArray = (anyArray) => {
     //Calling the generator function
     const generatorIterableObject = processArrayList(anyArray);
     const newFormatedArray = []
@@ -34,3 +34,5 @@ export const getIterableArray = (anyArray: Array<any>) : Array<any> => {
     }
     return newFormatedArray;
 }
+
+module.exports.getIterableArray = getIterableArray;
